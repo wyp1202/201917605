d18fc70eb0d6079859f55db43e54127f84c7b468
wyp1202 <117979262+wyp1202@users.noreply.github.com>
2022-11-11 17:26:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+header("Tel: 13824607587");
+?>
+<!DOCTYPE HTML>
+<!-- fork: https://github.com/lingyired/Web-resume -->
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
+<title>刘樟宏的个人简历</title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<!--[if lt IE 9]>
+   <script src="js/html5.js"></script>
+   <script src="js/css3-mediaqueries.js"></script>
+<![endif]-->
+</head>
+<body>
+<div class="main width" itemscope="" itemtype="http://schema.org/Person">
+		<header id="info" class="clearfix header">
+			<div class="info">
+				<h1><span itemprop="name">刘樟宏 (lingyired)</span></h1>
+				<h3><span itemprop="jobTitle">web前端工程师</span></h3>
+				<small>生于<span itemprop="birthDate">1990年4月</span>、<span itemprop="gender">男</span></small>
+				<small><span itemprop="email"><a href="mailto:lingyired@Gmail.com" target="_blank">lingyired@gmail.com</a></span></small>
+			</div>		
+			<div class="photo">
+				<img src="img/me.png"  itemprop="image">
+			</div>	
+		</header>
+
+		<section id="profile" class="section">
+			<h2>个人情况</h2>
+			<p itemprop="description">目前是肇庆学院大四学生，今年六月份毕业。现居住于深圳。</p>
+			<p>从2010年建立个人独立博客以及UbuntuSoft 起就开始关注和自学Web 相关技能。使用（试用）过众多流行的PHP 开源程序，但比较熟悉的只有Wordpress、PHPCMS、Discuz，并且有基于WP/PC/DZ 设计、架设站点的经历。</p>
+			<p>于2012年7月开始确定往Web 前端方向发展。</p>
+			<p>2013年1月基于ThinkPHP 开发了UbuntuSoft（Ubuntu 软件中心网页版）。<a href="http://get.ubuntusoft.com/" target="_blank">预览</a></p>
+			<p>常用开发环境为Ubuntu + Sublime Text 2 + Chromium</p>
+		</section>
+
+		<section id="skills" class="section">
+			<h2>技能</h2>
+			<ul>
+				<li class="skill">
+					<div class="skill_level" style="width:70%;opacity: 0.6;">HTML5/CSS3</div>
+				</li>
+				<li class="skill">
+					<div class="skill_level" style="width:70%;opacity: 0.6;">原生JS</div>
+				</li>
+				<li class="skill">
+					<div class="skill_level" style="width:90%;opacity: 0.85;">JS框架(jQuery)</div>
+				</li>
+				<li class="skill">
+					<div class="skill_level" style="width:93%;opacity: 0.88;">前端框架(Bootstrap / jQuery UI)</div>
+				</li>
+				<li class="skill">
+					<div class="skill_level" style="width:75%;opacity: 0.62;">移动Web(jQuery Mobile)</div>
+				</li>
+				<li class="skill">
+					<div class="skill_level" style="width:70%;opacity: 0.6;">后端(LAMP / LNMP)</div>
+				</li>
+				<li class="skill">
+					<div class="skill_level" style="width:80%;opacity: 0.65;">运营相关(SEO / SMO / 编辑)</div>
+				</li>
+			</ul>
+		</section>
+
+		<section id="experience" class="section">
+			<h2>项目经验</h2>
+			<p><a href="http://blog.ubuntusoft.com/" target="_blank">UbuntuSoft Blog</a> ~ 2010.7-至今</p>
+			<p><a href="http://get.ubuntusoft.com/" target="_blank">UbuntuSoft</a> ~ 2013.1-至今</p>
+			<p><a href="http://linuxrss.org/" target="_blank">LinuxRSS</a> ~ 2012.7-至今</p>
+			<p>蛙盒子-在线应用平台(web站点) ~ 2011.7-至今</p>
+			<p>优惠卷管理应用(兼容平板和PC 的Web 应用) ~ 2012.11-2012.12</p>
+			<p>智能家居移动网页端(移动web、老师带队) ~ 2012.7-2012.10</p>
+		</section>	
+
+		<section id="education" class="section">
+			<h2>教育经历</h2>
+			<p>2009-2013 <span itemprop="alumniOf">肇庆学院</span></p>
+			<p>2006-2009 <span itemprop="alumniOf">深圳南头中学</span></p>
+			<p>2003-2006 <span itemprop="alumniOf">深圳蛇口中学</span></p>
+		</section>		
+
+		<section id="otherinfo" class="section">
+			<h2>其它信息</h2>
+				<h3>兴趣方向</h3>
+				<p>WebAPP > WebGame > MobileWeb</p>
+				<h3>希望进入的公司</h3>
+				<p>创业公司or大公司的新项目团队。希望有前端大牛教育。</p>
+		</section>	
+
+		<div class="line"><span>结束，感谢浏览</span></div>
+		<section id="message" class="section">
+			<p>如果您想进一步了解我，可以通过↓下面↓的联系方式联系我。</p>
+		</section>					
+</div>
+
+<div class="foot">
+	<div class="footbox width">
+		<p>lingyired @ ( <a href="http://v2ex.com/member/lingyired" target="_blank">V2EX</a> / <a href="http://weibo.com/lingyired" target="_blank">Weibo</a> / <a href="https://twitter.com/lingyired" target="_blank">Twitter</a> / <a href="https://github.com/lingyired" target="_blank">Github</a> / <a href="mailto:lingyired@Gmail.com" target="_blank">Gmail</a>) | <a href="http://lzh.im/" target="_blank">Blog</a></p>
+	</div>
+</div>
+</body>
